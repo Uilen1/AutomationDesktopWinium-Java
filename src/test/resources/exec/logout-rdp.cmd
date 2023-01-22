1e810feccf622d771fbc30ce4403a8bfc4f6a3cb
Uilen1 <uilenlelles@hotmail.com>
2023-01-22 11:32:25 -0300

    Alterando Jenkinsfile e incluindo arquivos executaveis

--- src/test/resources/exec/logout-rdp.cmd
+++ src/test/resources/exec/logout-rdp.cmd
@@ -1,1 +1,1 @@
-for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (%windir%\System32\tscon.exe %%s /dest:console
+for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (%windir%\System32\tscon.exe %%s /dest:console)
